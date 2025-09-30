32f4857017bc6fff74c4a8c101d93d303fbdc682
arman <arman.ozcelik@gmail.com>
2025-10-01 00:27:59 +0200

    testing

--- slyworks.js
+++ slyworks.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SAWORK
 // @namespace    http://tampermonkey.net/
-// @version      0.0.6
+// @version      0.0.9
 // @description  try to take over the world!
 // @author       SLY w/ Contributions by niofox, SkyLove512, anthonyra, [AEP] Valkynen, Risingson, Swift42
 // @match        https://*.based.staratlas.com/
