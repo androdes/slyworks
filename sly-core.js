e365aca44c68c6c5f57eabb1ffa6fd0a0af6a5aa
arman <arman.ozcelik@gmail.com>
2025-09-30 15:24:02 +0200

    init

---
+++ sly-core.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+    // Définir maVariable globalement
+    window.maVariable = "================Hello World=====================";
+
+    // Utiliser maVariable
+    console.log(maVariable); // Affiche : Hello World
+})();
